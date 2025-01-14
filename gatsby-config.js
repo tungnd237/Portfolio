61cf5c5178e382c79b9e63c8951399888a174ddf
tungnd237 <tungtorres23@gmail.com>
2025-01-14 01:05:38 -0800

    fix img

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Andrew Nguyen',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Andrew Nguyen is a software engineer who specializes in building exceptional digital experiences.',
+    siteUrl: 'https://andrew-nguyen-eda07.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Andrew Nguyen',
+        short_name: 'Andrew',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
